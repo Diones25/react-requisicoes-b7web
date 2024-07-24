ee5b4ac73b99363795f511514fbdffd6e5d0eb11
Diones Pereira Alves <pereiradiones987@gmail.com>
2024-07-24 13:11:45 -0300

    validação no react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
+import { SubmitHandler, useForm } from "react-hook-form"
+
+type Inputs = {
+  name: string;
+  lastName: string;
+  age: number;
+}
 
 const Page = () => {
+  const { handleSubmit, register } = useForm<Inputs>();
+
+  const handleFormSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data)
+  }
 
   return (
     <>
       <div className="container mx-auto max-w-lg px-2">
         
+        <form onSubmit={handleSubmit(handleFormSubmit)} className="mt-3">
+          <input
+            type="text"
+            {...register('name', { required: true, minLength: 2, maxLength: 20 })}
+            placeholder="Digite seu nome"
+            className="border border-white p-3 text-black"
+          />
+
+          <input
+            type="text"
+            {...register('lastName')}
+            placeholder="Digite seu sobrenome"
+            className="block mt-3 border border-white p-3 text-black"
+          />
+
+          <input
+            type="number"
+            {...register('age', { required: true, min: 18 })}
+            placeholder="Digite sua idade"
+            className="block mt-3 border border-white p-3 text-black"
+          />
+
+          <input
+            type="submit"
+            value="Enviar"
+            className="bg-blue-400 px-4 py-1 mt-3 rounded-sm cursor-pointer"
+          />
+
+        </form>
+
       </div>
     </>
   )
