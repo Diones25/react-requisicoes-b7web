4c4f64ec15442de37fd7d45cf6a617b74cb4e358
Diones Pereira Alves <pereiradiones987@gmail.com>
2024-07-24 14:06:08 -0300

    adicionando erros no arquivos Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,6 +13,9 @@ export const Input = (props: UseControllerProps<SignupForm>) => {
       />
       {fieldState.invalid && <p></p>}
       {fieldState.error?.type === 'required' && <p className="text-red-500">Campo obrigatório...</p>}
+      {fieldState.error?.type === 'minLength' && <p className="text-red-500">O nome deve ser maior que 2 caracteres...</p>}
+      {fieldState.error?.type === 'maxLength' && <p className="text-red-500">O nome deve ser menor que 20 caracteres...</p>}
+      {fieldState.error?.type === 'min' && <p className="text-red-500">Este item precisa ser no mínimo 18 anos</p>}
     </div>
   )
 }
